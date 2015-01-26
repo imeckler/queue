bb2ae5f528442e3c26f6b937d7a6b06c8c9b963b
Izaak Meckler <ihmeckler@gmail.com>
2015-01-26 00:11:12 -0600

    first commit

---
+++ Queue.elm
@@ -0,0 +1,44 @@
+module Queue (
+  Queue,
+  empty,
+  push,
+  pop,
+  isEmpty,
+  length,
+  map,
+  toList
+  ) where
+
+{-| Just a simple queue data type -}
+
+import List
+
+type Queue a = Queue (List a) (List a)
+
+empty : Queue a
+empty = []
+
+push : a -> Queue a -> Queue a
+push x (Queue f b) = (Queue f (x::b))
+
+pop : Queue a -> Maybe (a, Queue a)
+pop (Queue f b) = case f of
+  [] -> case b of
+    [] -> Nothing 
+    _  -> let x :: f' = List.reverse b in Just (x, Queue f' [])
+  x :: xs -> Just (x, Queue xs b)
+
+isEmpty : Queue a -> Bool
+isEmpty q = case q of
+  Queue [] [] -> True
+  _           -> False
+
+length : Queue a -> Int
+length (Queue f b) = List.length f + List.length b
+
+map : (a -> b) -> Queue a -> Queue b
+map g (Queue f b) = Queue (map g f) (map g b)
+
+toList : Queue a -> List a
+toList (Queue f b) = f ++ List.reverse b
+
